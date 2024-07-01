515f8ffbc8bb3a4df6da45483d8fb0168559d3b4
itcast <hello@itcast.cn>
2024-07-01 11:00:11 +0800

    第九次修改提交20240701test01

--- src/main/java/HelloTest01.java
+++ src/main/java/HelloTest01.java
@@ -1,10 +1,10 @@
 public class HelloTest01 {
     public static void main(String[] args) {
         System.out.println("提交到branch04 并合并到主分支");
-
-
-
         System.out.println("再次提交 提交至branch05 并合并到主分支");
         System.out.println("提交到branch05 并合并到主分支");
+
+
+        System.out.println("提交到branch07 并合并到主分支");
     }
 }
