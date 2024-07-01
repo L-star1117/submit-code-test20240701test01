96b88562e8bb8db4c2b667f8fc837f6554f780f5
itcast <hello@itcast.cn>
2024-07-01 10:33:42 +0800

    第六次修改提交20240701test01

--- src/main/java/HelloTest01.java
+++ src/main/java/HelloTest01.java
@@ -1,5 +1,5 @@
 public class HelloTest01 {
     public static void main(String[] args) {
-        System.out.println("hello world test04");
+        System.out.println("提交到branch04 并合并到主分支");
     }
 }
--- src/main/java/HelloTest03.java
+++ src/main/java/HelloTest03.java
@@ -1,5 +1,5 @@
 public class HelloTest03 {
     public static void main(String[] args) {
-        System.out.println("helloworldtest02");
+        System.out.println("helloworldtest03");
     }
 }
