15d0154b03a8eb1f8931fcb3c815f07ac8a369ad
itcast <hello@itcast.cn>
2024-07-01 10:26:58 +0800

    第四次修改提交20240701test01

--- src/main/java/HelloTest01.java
+++ src/main/java/HelloTest01.java
@@ -1,5 +1,5 @@
 public class HelloTest01 {
     public static void main(String[] args) {
-        System.out.println("hello world test03");
+        System.out.println("hello world test04");
     }
 }
